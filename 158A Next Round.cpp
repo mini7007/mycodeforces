ec5fdb821af2097a77b5ae0b017d789d4193037e
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2025-01-05 14:47:25 +0530

    code updated for same with some modification

--- 158A Next Round.cpp
+++ 158A Next Round.cpp
@@ -1,50 +1,34 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main(){
-  int n;
-  int k;
-  int count;
-  cin>>n;
-  cin>>k;
-  int arr[n];
-  for(int i = 0; i < n; i++){
-    cout<<"i:"<<i<<" ";
-    cin>>arr[i];
-  }
-  int quali = 0, m;
-  if(arr[k-1] == 0){
+int main()
+{
+    int n, k;
+    cin >> n >> k;
 
-    for(m = k-2; m >= 0; m--){
+    int a;
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> a;
+        if (a == 0)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
 
-      if(arr[m] > 0){
-      
-        quali = arr[m];
-        count  = m+1;
-        cout<<count;
-        return 0;
-      }
+    int b;
+    for (int i = k; i < n; ++i)
+    {
+        cin >> b;
+        if (b != a)
+        {
+            cout << i << endl;
+            return 0;
+        }
     }
-    if(quali == 0){
-      cout<<0;
-      return 0;
-    }
-  }
-  else{
-    quali = arr[k-1];
-    count = k;
-  }
-  cout<<endl;
-  int j = count-2;
-  while(true){
-    if(arr[j] == quali){
-      count++;
-    }
-    else{
-      break;
-    }
-    j--;
-  }
-  cout<<count;
-  return 0;
+
+    cout << n << endl;
+    return 0;
 }

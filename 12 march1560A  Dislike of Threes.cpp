5e0bbe41ea9fc068bc3a10953486c8557419a76a
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2025-03-12 22:12:30 +0530

    12 march done

---
+++ 12 march1560A  Dislike of Threes.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int k;
+        cin>>k;
+        int i=0,j=0;
+        while(j!=k)
+        {
+            i++;
+            if(i%3!=0 && i%10!=3)
+            j++;
+        }
+        cout<<i<<endl;
+    }
+    return 0;
+}

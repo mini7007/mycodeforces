790ea5a1c6d42276491ec8c9b57f3bf1f79f4369
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2025-04-13 13:58:13 +0530

    Create 1551 A Polycarp and Coins.cpp
    13 april done

---
+++ 1551 A Polycarp and Coins.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int c1=0, c2=0;
+        if(n%3==0)
+        {
+            cout<<n/3<<" "<<n/3<<endl;
+            continue;
+        }
+        if(n%3==1)
+        {
+            cout<<(n/3 + 1)<<" "<<n/3<<endl;
+            continue;
+        }
+        cout<<(n/3)<<" "<<(n/3 + 1)<<endl;
+    }
+    return 0;
+}

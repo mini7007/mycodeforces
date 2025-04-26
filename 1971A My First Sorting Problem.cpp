23fca51575a68640f456f618d10a88ae7f0ceb6b
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2025-04-26 15:15:32 +0530

    Create 1971A My First Sorting Problem.cpp
    26 april done

---
+++ 1971A My First Sorting Problem.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+
+int main()
+{   
+   ll t,a,b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        cout<<min(a,b)<<" "<<max(a,b)<<endl;
+        
+}
+}
